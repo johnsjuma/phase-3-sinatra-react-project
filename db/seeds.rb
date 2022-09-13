7c4b679498f46a0d15913004d4f50831bf2a2811
jerryjohns <jerry.johns@student.moringaschool.com>
2022-09-13 14:54:22 +0300

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,13 @@ service = %w!sauna whirlpool jetbath massage saltscrub clay waxing nailservices 
 for i in service
   SpaAttendant.create(
     first_name: Faker::Name.name,
-    last_name: Faker::Name.middle_name,
-    service_type: service[rand(0...service.size)],
+    last_name: Faker::Name.last_name,
+    gender: "M",
+    email:"learning@gmail.com", 
+    address:Faker::Address.street_address,
+    phone:Faker::PhoneNumber.phone_number,
+    town:Faker::Address.city ,
+    service_name: service[rand(0...service.size)],
     service_id: rand(1..11)
   )
   Customer.create(
